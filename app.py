c5cb55282218b79aa10d352a985fb6bfbd6cfb9c
Tara Denniston <taradenniston@gmail.com>
2022-11-29 16:48:28 -0500

    set up basic html structure for board

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask, render_template
 from boggle import Boggle
 
+app = Flask(__name__)
+
 boggle_game = Boggle()
+board = boggle_game.make_board()
+
+@app.route('/')
+def display_board():
+    return render_template('home.html', board=board)
