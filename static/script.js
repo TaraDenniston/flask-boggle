d83f9c17e0587c18f35e0487295aa6e894fd4c25
Tara Denniston <taradenniston@gmail.com>
2022-12-02 18:04:14 -0500

    added functionality to check if word is valid

--- static/script.js
+++ static/script.js
@@ -1,16 +1,43 @@
 console.log('starting new game')
 
-/* Get user-submitted word and make a request to server with AJAX  */
+/* Return a message based on a specific result string*/
 
-async function getWord(evt) {
+function returnMessage(result) {
+    msg = ''
+
+    if (result === 'ok') {
+        msg = 'Great job - you found a word!';
+    } else if (result === 'not-on-board') {
+        msg = 'That word is not on the board. Try again.';
+    } else if (result === 'not-word') {
+        msg = 'Sorry, that is not a word.';
+    } else {
+        msg = 'Please try again';
+    }
+
+    return msg;
+}
+
+/* Get user-submitted word and make a request to server with AJAX to 
+ * check if it is valid. Then display a message to the user based on 
+ * the results. */
+
+async function checkWord(evt) {
     evt.preventDefault();
     let word = $('#guess').val();
     $('#guess').val('');
 
     const response = await axios.get('/guess', {params: {word: word}})
     console.log(response);
+
+    // Figure out which message to display depending on the response
+    msg = returnMessage(response.data.result);
+
+    // Display the message
+    $('#message').text(msg);
+
 }
 
-$('#submit-word').on('submit', getWord);
 
-console.log($('#submit-word'))
+$('#submit-word').on('submit', checkWord);
+
