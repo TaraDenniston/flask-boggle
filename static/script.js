6f1b84604e4e101e2c99a5d5deae2de5a7b1ce81
Tara Denniston <taradenniston@gmail.com>
2022-12-15 07:31:05 -0500

    added timer

--- static/script.js
+++ static/script.js
@@ -1,4 +1,6 @@
-console.log('starting new game')
+let currentScore = 0;
+const wordList = new Set();
+let gameInProgress = false;
 
 /* Return a message based on a specific result string*/
 
@@ -18,6 +20,7 @@ function returnMessage(result) {
     return msg;
 }
 
+
 /* Get user-submitted word and make a request to server with AJAX to 
  * check if it is valid. Then display a message to the user based on 
  * the results. */
@@ -26,18 +29,64 @@ async function checkWord(evt) {
     evt.preventDefault();
     let word = $('#guess').val();
     $('#guess').val('');
+    let msg = '';
 
-    const response = await axios.get('/guess', {params: {word: word}})
-    console.log(response);
+    if (gameInProgress) {
+        console.log('checking word');
+        const response = await axios.get('/guess', {params: {word: word}})
+        console.log(response);
 
-    // Figure out which message to display depending on the response
-    msg = returnMessage(response.data.result);
+        // Figure out which message to display depending on the response
+        msg = returnMessage(response.data.result);
+
+        // If word is valid, update current score and add word to list of
+        // words found. If word has already been guessed, update the message 
+        // to notify user.
+        if (response.data.result === 'ok') {
+            if (!wordList.has(word)) {
+                currentScore += word.length;
+                $('#curr_score').text(currentScore);
+                wordList.add(word);
+                $('#words').append(`<div>${word}</div>`)
+                console.log(wordList);
+            } else if (wordList.has(word)) {
+                msg = "You've already found that word";
+            }
+        } 
+    } else {
+        msg = 'Game is not in progress - begin a game to start the timer'
+    }
 
     // Display the message
     $('#message').text(msg);
+}
 
+function beginTimer(evt) {
+    evt.preventDefault();
+    gameInProgress = true;
+    console.log('beginning timer')
+    $timerVal = $('#timer').text();
+    console.log($timerVal);
+
+    // Update the timer value every 1 second until it reaches 0
+    setInterval(function func() {
+        if ($timerVal <= 0) {
+            gameInProgress = false;
+        }
+        if (gameInProgress) {
+            $timerVal -= 1;
+            $('#timer').text($timerVal);
+        }
+    }, 1000);
 }
 
 
 $('#submit-word').on('submit', checkWord);
+$('#start-new-game').on('click', beginTimer);
+
+        // $timerVal = $('#timer').text()
+        // console.log($timerVal);
+        // if ( $timerVal === 60) {
+        //     console.log('begin timer');
+        //     beginTimer();
 
